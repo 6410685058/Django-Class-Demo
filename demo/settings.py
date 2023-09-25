755e288794239bc9a9cd1275d0585e2e62bc5d85
6410685058 <88650864+6410685058@users.noreply.github.com>
2023-09-25 03:07:45 +0000

    add1

--- demo/settings.py
+++ demo/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try :
+    from .local_setting import *
+except ImportError:
+    pass
